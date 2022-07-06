f0a6d259d1ce2d4a26b486a61d4ff911b1c5da2d
Conor Higgins <wodowods@gmail.com>
2022-07-06 14:38:14 +0100

    added capitalise function

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ const main = document.querySelector(".card-container")
 
 import {pokemonArray} from "../data/pokemon.js";
 
+
+const capitaliseWord = (word) => {
+
+    return word.substring(0,1).toUpperCase() + word.slice(1);
+}
+
 const addPokes = (arr) => {
 
 
     arr.forEach((poke) => {
 
-        let types = poke.types.join(",").replace(/,/g, "&").split();
-        console.log(poke.sprite);
+        let types = poke.types.join(",").replace(/,/g, " & ").split();
+        poke.name = capitaliseWord(poke.name);
 
-            main.innerHTML += ` 
-                                <div class="card">
-                                    <img src="${poke.sprite}" class="card__image">"
-                                    <p class="card__heading">${poke.name}</p>
-                                    <p class="card__text">${poke.name} (#${poke.id}) is a ${types} type Pokemon.</p>
-                                </div>`
+        main.innerHTML += ` <div class="card">
+                                <img src="${poke.sprite}" class="card__image">"
+                                <p class="card__heading">${poke.name}</p>
+                                <p class="card__text">${poke.name} (#${poke.id}) is a ${types} type Pokemon.</p>
+                            </div>`
     })
 
 }

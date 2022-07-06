4f4086eb9c003bbd438f0ab804e74f42b3ef3b80
Conor Higgins <wodowods@gmail.com>
2022-07-06 15:38:24 +0100

    initial searchbar functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const main = document.querySelector(".card-container")
 import {pokemonArray} from "../data/pokemon.js";
-
+const searchbar = document.querySelector(".search")
+let currSearch;
+let currPokes = [];
 
 const capitaliseWord = (word) => {
 
@@ -13,14 +15,75 @@ const addPokes = (arr) => {
         let types = poke.types.join(",").replace(/,/g, " & ").split();
         poke.name = capitaliseWord(poke.name);
 
-        main.innerHTML += ` <div class="card">
-                                <img src="${poke.sprite}" class="card__image">
-                                <p class="card__content card__heading">${poke.name}</p>
-                                <p class="card__content card__text">${poke.name} (#${poke.id}) is a ${types} type Pokemon.</p>
-                            </div>`
+        if(!currPokes.includes(poke.name)) {
+            currPokes.push(poke.name);
+            main.innerHTML += ` <div class="card">
+                                    <img src="${poke.sprite}" class="card__image">
+                                    <p class="card__content card__heading">${poke.name}</p>
+                                    <p class="card__content card__text">${poke.name} (#${poke.id}) is a ${types} type Pokemon.</p>
+                                </div>`
+        }
     })
 
 }
 
 
-addPokes(pokemonArray)
+const removePokes = (arr) => {
+
+    if(arr.length != 0) {
+
+            arr.forEach((poke) => {
+
+                let types = poke.types.join(",").replace(/,/g, " & ").split();
+                if(arr.includes(poke.name)) {
+                    arr.pop(poke.name);
+                    main.innerHTML -= ` <div class="card">
+                                            <img src="${poke.sprite}" class="card__image">
+                                            <p class="card__content card__heading">${poke.name}</p>
+                                            <p class="card__content card__text">${poke.name} (#${poke.id}) is a ${types} type Pokemon.</p>
+                                        </div>`
+                }
+            })
+    }
+}
+
+const searchPoke = (searchString, arr) => {
+
+
+    currSearch = String(searchString);
+    let currSearchLength = currSearch.length;
+    let searchArr = [];
+
+    console.log("current search:" + currSearch);
+
+    arr.forEach((poke) => {
+
+        if(currSearch.includes(poke.name.substring(0,currSearchLength))) {
+
+            searchArr.push(poke);
+            currPokes = [...searchArr];
+            
+        }
+    });
+
+    console.log("searchArr:" + searchArr);
+    return addPokes(searchArr);
+    
+
+}
+
+searchbar.addEventListener("input", () =>{
+
+    console.log(searchbar.value.length);
+
+    if(searchbar.value.length != 0)
+    {
+        searchPoke(searchbar.value, pokemonArray); 
+        
+    } else if(currPokes.length > 0) {
+        removePokes(currPokes);
+    }
+
+});
+
+//if(!currSearch){addPokes(pokemonArray)};
